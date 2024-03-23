2aae65f1650d190137c5b9bcdb30393266f2a3f4
Abby H <howe.abigail@gmail.com>
2024-03-23 15:29:57 -0500

    Added modal event listener, Dialog box to add ideas

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,11 @@
-import '@fortawesome/fontawesome-free';
+import '@fortawesome/fontawesome-free/css/all.css';
 import './css/style.css';
+
+const modal = document.querySelector('#modal');
+const modalBtn = document.querySelector('#modal-btn');
+
+function open() {
+  modal.style.display = 'block';
+}
+
+modalBtn.addEventListener('click', open);
